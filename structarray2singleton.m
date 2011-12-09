11c881f42a6897b35cbe0c8ee30965a1fb1cdb8e
Mark Histed <mhisted@gmail.com>
2011-12-09 18:12:54 -0500

    update struct conversion ops, add figs_of_subplots

--- structarray2singleton.m
+++ structarray2singleton.m
@@ -2,13 +2,11 @@ function singleStruct = structarray2singleton(structArrIn)
 %
 %  Note that this uses cell2mat on each field to make it a single matrix, so
 %  it is subject to the limitations of cell2mat.  
-%       Specifically, if there are any field elements that are empty, they will be
-%       left out of the resulting big matrix (it will be smaller).  Fill with
-%       NaN's if you care about this.
+%
+%  However, we first look for double matrices with empty entries; empty is
+%  padded with NaN.
 %
 %  MH - http://github.com/histed/tools-mh
-
-error('not tested well, verify')
 
 anonCell = struct2cell(structArrIn);
 fNames = fieldnames(structArrIn);
@@ -18,8 +16,34 @@ structArrSize = anonCellSize(2:end);
 
 for iF=1:nFields
     tFieldName = fNames{iF};
+    
     tFieldVals = reshape(anonCell(iF,:), structArrSize); %handle: multi-dims
                                                          %(of orig struct arr)    
-    singleStruct.(tFieldName) = cell2mat(tFieldVals);
+    isDIx = cellfun(@isfloat, tFieldVals);
+    isEIx = cellfun(@isempty, tFieldVals);
+    isSIx = cellfun(@isstruct, tFieldVals);
+    if all(isDIx(:) | isEIx(:))
+        % every cell is a double or empty, fill with NaN
+        if all(isEIx(:))
+            % all empty
+            singleStruct.(tFieldName) = []; 
+            continue;
+        else
+            % some el numeric, some empty
+            [tFieldVals{isEIx}] = deal(NaN);
+        end
+        
+        singleStruct.(tFieldName) = cell2mat(tFieldVals);
+    elseif all(isSIx(:) | isEIx(:))
+        % structure, call recursively
+        
+        if all(isSIx(:))
+            singleStruct.(tFieldName) = structarray2singleton(tFieldVals);
+        end
+    else
+        % copy directly
+        singleStruct.(tFieldName) = tFieldVals;
+    end
+
 end
 
--- structincell2structarray.m
+++ structincell2structarray.m
@@ -1,13 +1,18 @@
-function structArray = structincell2structarray(cellArrayIn)
+function structArray = structincell2structarray(cellArrayIn, fillVal)
 %STRUCTINCELL2STRUCTARRAY (ps-utils): concat structures inside cell array
-%   structArray = STRUCTINCELL2STRUCTARRAY(cellArrayIn)
+%   structArray = STRUCTINCELL2STRUCTARRAY(cellArrayIn, fillVal)
 % 
-%   cellArrayIn must have a single structure in each cell.  Each
-%   structure must have the same fields.  If a cell contains NaN (the
+%   cellArrayIn must have a single structure in each cell.  
+%   If a cell contains NaN (the
 %   singleton numeric matrix), all fields of the struct for that cell are set
-%   to NaN's
+%   to fillVal.
+%   fillVal defaults to [], NaN is also useful
+%
+%   If fields are different, corresponding entries have empty field values
 %
 %  MH - http://github.com/histed/tools-mh
+
+fillVal = [];
 
 % find 1x1 NaN numeric matrices in CellArrayIn 
 nonStructIx = ~cellfun('isclass', cellArrayIn, 'struct');
@@ -16,17 +21,49 @@ assert(all(isnumeric(nonStructVect)) && all(isnan(nonStructVect)), ...
        'Expect all non-struct values in cell array to be NaNs !');
 nanIx = nonStructIx;
 
+
+% replace singleton nans with the filler struct
+%filledCell = cellArrayIn;
+%filledCell(nanIx) = { fillerStruct };
+
+% find field names for each
+structCell = cellArrayIn(~nanIx);
+
+% find added field names
+fN1 = fieldnames(structCell{1}); % optimization
+fNameAdd = cellfun(@(x) setdiff(fieldnames(x),fN1), ...
+                 structCell, 'UniformOutput', false);
+% subtracted
+fNameSub = cellfun(@(x) setdiff(fN1, fieldnames(x)), ...
+                   structCell, 'UniformOutput', false);
+addNames = unique(cat(1,fNameAdd{:}));
+subNames = unique(cat(1,fNameSub{:}));
+for iC=1:length(structCell)
+    tExtras = setdiff(addNames, fNameAdd{iC});
+    tMissing = fNameSub{iC};
+    tS = structCell{iC};
+    tList = union(tExtras,tMissing);
+    for iE = 1:length(tList)
+        tS.(tList{iE}) = fillVal; 
+    end
+    structCell{iC} = orderfields(tS);
+end
+
+
+sortC = cellfun(@(x) x, structCell);  % dummy fn to return the structure
+
 % make a filler struct where all fields are NaN
-structFNames = fieldnames(cellArrayIn{1});
+structFNames = fieldnames(sortC);
 nFNames = length(structFNames);
 for iF=1:nFNames
     tFName = structFNames{iF};
-    fillerStruct.(tFName) = NaN;
+    fillerStruct.(tFName) = fillVal;
 end
 
-% replace singleton nans with the filler struct
-filledCell = cellArrayIn;
-filledCell(nanIx) = { fillerStruct };
+% replace blanks with fill struct
+outS(1) = sortC(1);
+outS(find(~nanIx)) = sortC(:);
+outS(nanIx) = deal(fillerStruct);
+structArray = outS;
 
-% now convert cell array to structure array
-structArray = cell2mat(filledCell);
+
