dae68811cbf41c33a21e6738966bb21873c1ff1e
Mark Histed <mhisted@gmail.com>
2012-05-25 18:12:37 -0400

    handle ticklabels in an improved way : add yaltman's ticklabelformat which deals correctly when axes are updated.  Changes to ticklabel_exp2float

--- ticklabel_exp2float.m
+++ ticklabel_exp2float.m
@@ -9,25 +9,31 @@ function ticklabel_exp2float(axH, axisStr)
 %   i.e. when initially setting XScale to log, tick labels may be 10^-1,
 %   10^0, 10^1; this function will make them 0.1, 1, 10
 %
+%   This must be called after any changes to limits/ticks are made.
+%
 % created: histed 110903
 
 if isempty(axH), axH = gca; end
 if nargin < 2, error('Must specify axisStr'); end
 
-switch lower(axisStr)
-  case 'x'
-    tStr = 'XTick';
-  case 'y'
-    tStr = 'YTick';
-  case 'z'
-    tStr = 'ZTick';
-  otherwise
-    error(sprintf('Invalid axisStr: %s', axisStr));
-end
-tLStr = [ tStr 'Label' ];
+% this util uses a listener to deal with zooms/tick changes
+ticklabelformat(axH, lower(axisStr), '%g');
 
-% almost ridiculously simple
-tTL = cellstr(num2str(get(axH, tStr)', 4));
-set(axH, tLStr, tTL);
-    
-    
+% switch lower(axisStr)
+%   case 'x'
+%     tStr = 'XTick';
+%   case 'y'
+%     tStr = 'YTick';
+%   case 'z'
+%     tStr = 'ZTick';
+%   otherwise
+%     error(sprintf('Invalid axisStr: %s', axisStr));
+% end
+% tLStr = [ tStr 'Label' ];
+% 
+% % almost ridiculously simple
+% tTL = cellstr(num2str(get(axH, tStr)', 4));
+% set(axH, tLStr, tTL);
+% 
+% %% lock props
+% lock_handle_properties(axH,  { tStr, tLStr, strrep(tStr, 'Tick', 'Lim') }, mfilename);
