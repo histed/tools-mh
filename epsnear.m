e90268a98c82fed04d0439e6ee4ba4a43470f115
Mark Histed <mhisted@gmail.com>
2014-06-17 14:13:02 -0400

    epsnear: use relative difference
    Signed-off-by: Mark Histed <mhisted@gmail.com>

--- epsnear.m
+++ epsnear.m
@@ -1,12 +1,15 @@
 function bool = epsnear(float1, float2, epsilon)
 %EPSNEAR (ps-utils): are two floating point numbers equal?
 %   BOOL = ISWITHINTOL(FLOAT1, FLOAT2, EPSILON)
-%   returns 1 if arguments differ by less than epsilon
+%   returns 1 if arguments' relative difference is less than epsilon
 %
 %   EPSILON defaults to eps x 10^2
 %
+%  140614: use relative difference
 %  MH - http://github.com/histed/tools-mh
+
 if nargin < 3, epsilon = eps*10^2; end
 
-bool = ( abs(float1 - float2) < epsilon);
+m = (float1+float2)./2;
+bool = ( abs( (float1-float2)./m) < epsilon);
 
