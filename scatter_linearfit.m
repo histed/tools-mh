7e52a4130a553c2cf5d40b4ef287eba70510c792
Mark Histed <mhisted@gmail.com>
2015-07-07 10:42:59 -0500

    scatter_linearfit - can draw CI patches on slope/intercept now
    Signed-off-by: Mark Histed <mhisted@gmail.com>

--- scatter_linearfit.m
+++ scatter_linearfit.m
@@ -1,6 +1,7 @@
 function [hand, b0, b1, r2, pval] = scatter_linearfit (xdata, ydata, robust, ...
                                                        plot_params, text_to_add, ...
-                                                       line_xdata)
+                                                       line_xdata, do_draw_cis, ...
+                                                       ci_alpha)
 %SCATTER_LINEARFIT (ps-utils): add regression line to scatterplot
 %   [HAND, B0, B1, R2] = SCATTER_LINEARFIT (XDATA, YDATA, ROBUST, PLOT_PARAMS)
 %   You must draw the scatter plot yourself
@@ -15,10 +16,10 @@ function [hand, b0, b1, r2, pval] = scatter_linearfit (xdata, ydata, robust, ...
 if nargin < 3 || isempty(robust), robust = 'leastsq'; end
 if nargin < 4 || isempty(plot_params), plot_params = {'k'}; end
 if nargin < 5 || isempty(text_to_add), text_to_add = { 'slope', 'offset', 'r^2', 'p' }; end
-if nargin < 6 || isempty(line_xdata), line_xdata = [min(xdata) max(xdata)]; end  
-
-
-
+if nargin < 6 || isempty(line_xdata), line_xdata = linspace(min(xdata),max(xdata)); end  
+if nargin < 7 || isempty(do_draw_cis), do_draw_cis = false; end
+if nargin < 8 || isempty(ci_alpha), ci_alpha = 5; end
+assert(ci_alpha == 5, 'code only draws 95% CIs now');
 
 xdata = squeeze(xdata);  % scatter.m does this (or at least shows this
 ydata = squeeze(ydata);  % behavior),  so we should too
@@ -36,54 +37,86 @@ if ~ischar(robust)
 end
 
 switch robust
-  case 'robust'
-    [b,st] = robustfit(X(:,[2:end]), ydata);
-    pval = st.p(2);
-    r = st.coeffcorr(2);
-    
-    tss = nansum( st.w .* (ydata - nanmean(ydata)) .^ 2);
-    yhat = b(1) + xdata * b(2);
-    ess = nansum( st.w .* (ydata - yhat) .^ 2);
-    r2 =  1 - ess / tss;
-  case 'leastsq'
-    [b crap1 crap2 crap3 stats] = regress(ydata,X);
-    pval = stats(3);
-    r2 = stats(1);
-    r = sqrt(r2) .* sign(b(2));
-  case 'orthogonal'
-    % take out each mean
-    xM = mean(xdata);
-    yM = mean(ydata);
-    yd0 = ydata - yM;
-    xd0 = xdata - xM;
-    
-    X = [xd0 yd0];
-    [coeff,score,roots] = princomp(X);
-    
-    % eq for best fit line
-    eig1 = coeff(:,1);
-    b(2) = eig1(2)/eig1(1);  % slope of best fit line is given by first
-                             % eigenvector direction
-    b(1) = yM - b(2)*xM;     % offset given by inverting y = mx+b
-
-    pctVar = roots' ./ sum(roots);
-    r2 = pctVar(1);
-    
-    [ndim,pv] = barttest(X, 0.9999);  % use high alpha, we don't care
-                                      % about ndims
-    
-    pval = pv;
-    
-otherwise
+    case 'robust'
+        rStr = 'on';
+    case 'leastsq'    
+        rStr = 'off';
+    case 'orthogonal'
+        assert('error, this code needs to be checked');
+        % take out each mean
+        xM = mean(xdata);
+        yM = mean(ydata);
+        yd0 = ydata - yM;
+        xd0 = xdata - xM;
+        
+        X = [xd0 yd0];
+        [coeff,score,roots] = princomp(X);
+        
+        % eq for best fit line
+        eig1 = coeff(:,1);
+        b(2) = eig1(2)/eig1(1);  % slope of best fit line is given by first
+        % eigenvector direction
+        b(1) = yM - b(2)*xM;     % offset given by inverting y = mx+b
+        
+        pctVar = roots' ./ sum(roots);
+        r2 = pctVar(1);
+        
+        [ndim,pv] = barttest(X, 0.9999);  % use high alpha, we don't care
+        % about ndims
+        
+        pval = pv;
+    otherwise
     error('Invalid value for robust parameter');
 end
+
+%mdl = fitlm(X(:,[2:end]), ydata, 'robust', rStr);
+mdl = LinearModel.fit(X(:,[2:end]), ydata, 'robust', rStr);
+pval = coefTest(mdl);
+b = mdl.Coefficients.Estimate;
+bint = mdl.coefCI;
+r2 = mdl.Rsquared.Ordinary;
+%keyboard
+%r2 = mdl.
+
+%         mdl = fitlm(X(
+%     %[b,st] = robustfit(X(:,[2:end]), ydata);
+%     pval = st.p(2);
+%     r = st.coeffcorr(2);
+%     
+%     tss = nansum( st.w .* (ydata - nanmean(ydata)) .^ 2);
+%     yhat = b(1) + xdata * b(2);
+%     ess = nansum( st.w .* (ydata - yhat) .^ 2);
+%     r2 =  1 - ess / tss;
+%     se = st.se;
+%     assert(ci_alpha == 5, 'only 95% CIs now');
+%     bint = [b + se*1.96; b - se*1.96];
+%   case 'leastsq'
+%     [b bint crap2 crap3 stats] = regress(ydata,X);
+%     pval = stats(3);
+%     r2 = stats(1);
+%     r = sqrt(r2) .* sign(b(2));
+% 
+    
+    
+
 
 b0 = b(1);
 b1 = b(2);    
 
 %xrange = [min(xdata) max(xdata)];
-
 hand(1) = plot(line_xdata, line_xdata*b1 + b0, plot_params{:});
+if do_draw_cis
+    hand(1) = plot(line_xdata, line_xdata*b1 + b0, plot_params{:});
+    [ypred, yci] = predict(mdl, line_xdata', 'Simultaneous', 'on');
+    pColor = [145 84 93]/255;
+    pH=patch([line_xdata(:)', fliplr(line_xdata(:)')], [yci(:,1)' fliplr(yci(:,2)')], pColor);
+    set(pH, 'EdgeColor', 'none');
+    anystack(pH, 'bottom');
+    hand(3) = pH;
+    %upY = 
+    %yP = [lowX
+    %hand(1) = patch(
+end
 
 % label plot
 tStr = [];
