b59c656229d0b47d786becf5db52b7ba8deebbce
Mark Histed <mhisted@gmail.com>
2015-07-22 17:38:02 -0500

    mh_figpreppdf
    Signed-off-by: Mark Histed <mhisted@gmail.com>

--- mh_figpreppdf.m
+++ mh_figpreppdf.m
@@ -1,17 +1,31 @@
-function mh_figpreppdf(figH)
+function mh_figpreppdf(figH, figSize, profileStr)
 % MH_FIGPREPPDF:  common plot fixups for pdf export
+if isempty(figSize), figSize = 6*[1 0.75]; end
+if isempty(profileStr), profileStr = 'working'; end   % or final
+
 
 axH = findobj(figH, 'Type', 'axes');
 
+switch profileStr
+    case 'working'
+        labelTextSize = 7;
+    case 'final'
+        labelTextSize = 6;
+    otherwise
+        error('unknown profileStr %s', profileStr);
+end
+
 set(axH, .... 
     'TickDir', 'out', ...
-    'TickLength', 0.012*[1 1], ...
+    ...    %'TickLength', figSize ./ 200, ... % i.e. 0.015 for a 3x3 figure
+    'TickLength', 0.015 * [1 1], ...% really need to figure out the number of subplots for this one
     'Box', 'on', ...
     'LineWidth', 0.25, ...
     'FontName', 'Arial', ...
-    'FontSize', 7);
+    'FontSize', labelTextSize);
 
 tH = findall(gcf, 'Type', 'text');
 set(tH, 'FontName', 'arial', ...
-    'fontsize', 8);
+    'fontsize', labelTextSize+1);
+
 
